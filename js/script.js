6550b0f4757efb5d417ebdba41ddefa8d15d694b
maelc <castellan.mael@efrei.net>
2024-01-22 11:52:46 +0100

    work

---
+++ js/script.js
@@ -0,0 +1,96 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Charger les réservations lors du chargement de la page
+    chargerReservations();
+
+    // Fonction pour ajouter une réservation
+    window.ajouterReservation = function () {
+        const nomReservateur = document.getElementById('nomReservateur').value;
+        const commentaire = document.getElementById('commentaire').value;
+        const article = document.getElementById('article').value;
+
+        if (!article || !commentaire) {
+            alert('Veuillez remplir tous les champs avant d\'ajouter une réservation.');
+            return;
+        }
+        const data = JSON.stringify({
+            name: nomReservateur,
+            product: article,
+            comment: commentaire
+        });
+
+        ajouterReservationSurServeur(data);
+
+    };
+
+    // Fonction pour charger les réservations depuis le serveur
+    async function chargerReservations() {
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("http://localhost:3000/reservation", requestOptions)
+            .then(response => response.json())
+            .then(reservations => afficherReservations(reservations))
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    }
+
+    // Fonction pour ajouter une réservation sur le serveur
+    function ajouterReservationSurServeur(data) {
+        fetch('/ajouter-reservation', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+        })
+            .then(data => {
+                console.log(data);
+                chargerReservations(); // Rafraîchir la liste des réservations
+            })
+            .catch(error => {
+                console.log('Erreur lors de l\'ajout de la réservation:', error);
+                chargerReservations();
+            });
+    }
+    // Ajout de la fonction de suppression côté client
+   function deleteReservation(id) {
+       fetch(`/supprimer-reservation/${id}`, {
+           method: 'DELETE',
+       })
+           .then(response => response.json())
+           .then(data => {
+               console.log(data); // Affiche un message de succès
+               chargerReservations(); // Rafraîchir la liste des réservations
+           })
+           .catch(error => {
+               console.log('Erreur lors de la suppression de la réservation:', error);
+               chargerReservations();
+           });
+   }
+
+    // Fonction pour afficher les réservations dans le DOM
+    function afficherReservations(reservations) {
+        const reservationsList = document.getElementById('reservationsList');
+        reservationsList.innerHTML = ''; // Effacer la liste actuelle
+
+        reservations.forEach(reservation => {
+            const reservationItem = `
+        <div class="reservationItem" id="reservationItem-${reservation._id}">
+          <p><strong>Nom du réservateur:</strong> ${reservation.name}</p>
+          <p><strong>Article:</strong> ${reservation.product}</p>
+          <p><strong>Commentaire:</strong> ${reservation.comment}</p>
+          <button class="supprimerReservationBtn" data-id="${reservation._id}">Supprimer</button>
+        </div>`;
+            reservationsList.insertAdjacentHTML('beforeend', reservationItem);
+        });
+
+        document.querySelectorAll('.supprimerReservationBtn').forEach(button => {
+            button.addEventListener('click', () => deleteReservation(button.dataset.id));
+        });
+    }
+
+
+
+});
---
+++ server.js
@@ -0,0 +1,87 @@
+// Import des modules
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+// Initialisation de l'application Express
+const app = express();
+
+// Configuration de body-parser pour traiter les données JSON
+app.use(express.json())
+app.use('/css', express.static('css'))
+app.use('/js', express.static('js'))
+const generateMain = require("./getMainHtml.js")
+
+
+
+// Connexion à la base de données MongoDB (assurez-vous d'avoir MongoDB installé localement)
+async function connect() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/decathlon');
+    } catch (error) {
+        console.log(error);
+    }
+}
+connect();
+
+
+// Définition du modèle pour les articles réservés
+const reservationSchema = new mongoose.Schema({
+    name: String,
+    product: String,
+    comment: String,
+});
+const Reservation = mongoose.model('reservation', reservationSchema);
+
+
+app.get('/',async (req, res) => {
+    const main = await generateMain();
+    res.send(main);
+})
+
+// Route pour ajouter une réservation
+app.post('/ajouter-reservation', (req, res) => {
+    console.log(req.body);
+
+    const nom = req.body.name;
+    const commentaire = req.body.comment;
+    const produit = req.body.product;
+    const nouvelleReservation = new Reservation({
+        name : nom,
+        product : produit,
+        comment : commentaire,
+    });
+    try {
+        nouvelleReservation.save();
+        res.status(200).send();
+    }catch (error){
+        console.log(error);
+        res.status(500).send();
+    }
+
+
+});
+
+// Route pour récupérer toutes les réservations
+app.get('/reservation', async (req, res) => {
+    const data = await Reservation.find();
+
+    res.send(data);
+});
+
+// Ajout de la route DELETE côté serveur
+app.delete('/supprimer-reservation/:id', async (req, res) => {
+    const reservationId = req.params.id;
+    console.log(reservationId);
+    const result = await Reservation.findByIdAndDelete(reservationId);
+    res.status(200).send();
+});
+
+
+
+
+// Port d'écoute du serveur
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`serveur démarré : http://localhost:${port}`)
+});
